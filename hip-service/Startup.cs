5c1b2799d2eef8398e7ded86aa36b885341f6a49
ganesan.arunachalam <aganesan@thoughtworks.com>
2019-12-23 19:36:23 +0530

    Checking swagger integration

--- hip-service/Startup.cs
+++ hip-service/Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.StaticFiles;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace hip_service
 {
@@ -28,6 +31,13 @@ namespace hip_service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".yaml"] = "application/x-yaml";
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+                ContentTypeProvider = provider
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -37,8 +47,10 @@ namespace hip_service
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseMvc();
+            app.UseSwaggerUI(actions => {
+                actions.SwaggerEndpoint("/swagger.yaml", "API docs");
+            });
         }
     }
 }
