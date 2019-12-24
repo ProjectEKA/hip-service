34103114e49c12f1c2dbecd27a56088da696432e
ganesan.arunachalam <aganesan@thoughtworks.com>
2019-12-24 11:14:00 +0530

    Upgrade aspnet core version integrate open api specification with redoc

--- hip-service/Startup.cs
+++ hip-service/Startup.cs
@@ -5,40 +5,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace hip_service
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services) =>
+            services
+                .AddSingleton<IPatientRepository>(new PatientRepository("Resources/patients.json"))
+                .AddTransient<IDiscovery, PatientDiscovery>()
+                .AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSingleton<IPatientRepository>(new PatientRepository("Resources/patients.json"));
-            services.AddTransient<IDiscovery, PatientDiscovery>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseMvc();
-        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) =>
+            app.UseStaticFilesWithYaml()
+            .UseRouting()
+            .UseIf(!env.IsDevelopment(), x => x.UseHsts())
+            .UseIf(env.IsDevelopment(), x => x.UseDeveloperExceptionPage())
+            .UseCustomOpenAPI();
     }
 }
