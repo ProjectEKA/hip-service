b011ec07f6bed7f3be7f6e8c6f8c08688cd0f970
nexengineer <nexengineer@gmail.com>
2019-12-30 16:19:19 +0530

    [EKA-12] Adding link reference api Co-Authored By : Apoorva <apoorvaga29@gmail.com>

---
+++ hip-service/Link/Patient/LinkPatient.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using hip_library.Patient;
+using hip_library.Patient.models;
+using hip_library.Patient.models.dto;
+
+namespace hip_service.Link.Patient
+{
+    public class LinkPatient: ILink
+    {
+        private readonly Patient.ILinkPatientRepository linkPatientRepository;
+
+        public LinkPatient(ILinkPatientRepository linkPatientRepository)
+        {
+            this.linkPatientRepository = linkPatientRepository;
+        }
+
+        public Task<Tuple<PatientLinkReferenceResponse, Error>> LinkPatients(PatientLinkReferenceRequest request)
+        {
+            return linkPatientRepository.LinkPatient(request.PatientReferenceNumber,
+                request.CareContexts.Select(context => context.ReferenceNumber).ToArray());
+        }
+    }
+}
