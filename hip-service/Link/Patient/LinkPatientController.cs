f9bed725b18e5b2d9546c92b0495b845b7100b4f
nexengineer <nexengineer@gmail.com>
2019-12-31 14:55:12 +0530

    [EKA-12] Adding middleware for service

--- hip-service/Link/Patient/LinkPatientController.cs
+++ hip-service/Link/Patient/LinkPatientController.cs
@@ -20,7 +20,6 @@ namespace hip_service.Link.Patient
         [HttpPost]
         public async Task<ActionResult> LinkPatientCareContexts([FromHeader(Name = "X-ConsentManagerID")]string consentManagerId,
             [FromBody]PatientLinkReferenceRequest request)
-
         {
             var (linkReferenceResponse, error) = await linkPatient.LinkPatients(request);
 
