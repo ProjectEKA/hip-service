319372c9a461e00132fc881275f7f3d1a2979b0a
Angshuman Sarkar <angshuonline@gmail.com>
2020-01-09 12:36:46 +0530

    [EKA-7] Fixing codacy pointers

--- hip-service/Discovery/Patient/Helpers/FuzzyNameMatcher.cs
+++ hip-service/Discovery/Patient/Helpers/FuzzyNameMatcher.cs
@@ -6,8 +6,8 @@ namespace hip_service.Discovery.Patient.Helpers
     {
         public static int LevenshteinDistance(string s1, string s2)
         {
-            var string1 = s1 == null ? "" : s1.Trim().ToLower();
-            var string2 = s2 == null ? "" : s2.Trim().ToLower();
+            var string1 = s1 == null ? "" : s1.Trim().ToLowerInvariant();
+            var string2 = s2 == null ? "" : s2.Trim().ToLowerInvariant();
             int l1 = string1.Length;
             int l2 = string2.Length;
             int[,] d = new int[l1 + 1, l2 + 1];
@@ -22,21 +22,33 @@ namespace hip_service.Discovery.Patient.Helpers
                 return l1;
             }
 
-            for (int i = 0; i <= l1; i++)
+            for (int i = 0; i <= l1; i++) 
+            {
                 d[i, 0] = i;
+            }
             for (int j = 0; j <= l2; j++)
+            {
                 d[0, j] = j;
+            }
 
             for (int j = 1; j <= l2; j++)
+            {
                 for (int i = 1; i <= l1; i++)
+                {
                     if (string1[i - 1] == string2[j - 1])
+                    {
                         d[i, j] = d[i - 1, j - 1];  //no operation
+                    }
                     else
+                    {
                         d[i, j] = Math.Min(Math.Min(
                             d[i - 1, j] + 1,    //a deletion
                             d[i, j - 1] + 1),   //an insertion
                             d[i - 1, j - 1] + 1 //a substitution
                             );
+                    }
+                }
+            }
             return d[l1, l2];
         }
     }
