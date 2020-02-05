3e6b4f3e5cb3bbb58c77e23eeb59cae90c2afa17
ganesanarun <ganesan.arunachalam91@gmail.com>
2020-02-05 12:41:14 +0530

    Introduce get by patient id api

--- src/In.ProjectEKA.TMHHip/Discovery/PatientMatchingRepository.cs
+++ src/In.ProjectEKA.TMHHip/Discovery/PatientMatchingRepository.cs
@@ -3,10 +3,12 @@ namespace In.ProjectEKA.TMHHip.Discovery
     using System.Collections.Generic;
     using System.Linq;
     using System.Net.Http;
+    using System.Text;
     using System.Threading.Tasks;
     using HipLibrary.Patient;
     using HipLibrary.Patient.Model;
-    using Patient = HipLibrary.Patient.Model.Patient;
+    using Newtonsoft.Json;
+    using JsonSerializer = System.Text.Json.JsonSerializer;
 
     public class PatientMatchingRepository : IMatchingRepository
     {
@@ -17,22 +19,28 @@ namespace In.ProjectEKA.TMHHip.Discovery
             this.client = client;
         }
 
-        public Task<IQueryable<Patient>> Where(DiscoveryRequest request)
+        public async Task<IQueryable<HipLibrary.Patient.Model.Patient>> Where(DiscoveryRequest predicate)
         {
-              return Task.FromResult(new List<Patient>
-                        {
-                            new Patient
-                            {
-                                Identifier = "5",
-                                PhoneNumber = "8340289040",
-                                CareContexts = new List<CareContextRepresentation>
-                                {
-                                    new CareContextRepresentation("131", "National Cancer Program")
-                                },
-                                FirstName = "Ron",
-                                LastName = "Doe"
-                            }
-                        }.AsQueryable());
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:49699/patients/find");
+            request.Content = new StringContent(
+                JsonConvert.SerializeObject(new {mobileNumber = predicate.Patient.VerifiedIdentifiers.First().Value}),
+                Encoding.UTF8, "application/json");
+            var response = await client.SendAsync(request);
+            await using var responseStream = await response.Content.ReadAsStreamAsync();
+            var result = await JsonSerializer.DeserializeAsync<IEnumerable<Patient>>(responseStream);
+            return result.Select(patient => new HipLibrary.Patient.Model.Patient
+            {
+                FirstName = patient.FirstName,
+                Gender = patient.Gender,
+                Identifier = patient.Identifier,
+                LastName = patient.LastName,
+                CareContexts = new List<CareContextRepresentation>
+                {
+                    new CareContextRepresentation($"{patient.Identifier}",
+                        $"{patient.FirstName}  {patient.LastName}")
+                },
+                PhoneNumber = "8340289040"
+            }).AsQueryable();
         }
     }
 }
