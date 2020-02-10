ec3f2d9e5c29aedc0c760c2843591e9ee296d093
nexengineer <nexengineer@gmail.com>
2020-02-10 12:15:07 +0530

    Codacy Fixes

--- test/In.ProjectEKA.HipServiceTest/DataFlow/Builder/TestBuilder.cs
+++ test/In.ProjectEKA.HipServiceTest/DataFlow/Builder/TestBuilder.cs
@@ -23,26 +23,6 @@ namespace In.ProjectEKA.HipServiceTest.DataFlow.Builder
         internal static Faker<ConsentArtefact> ConsentArtefact()
         {
             return new Faker<ConsentArtefact>();
-        //     var consentPurpose = new ConsentPurpose("Encounter", "ENCOUNTER","Some");
-        //     var patientReference = new PatientReference("25@ncg");
-        //     var hipReference = new HIPReference("10000005", "Xyz centre" );
-        //     var hiTypes = new List<HiType> {HiType.Condition};
-        //     var consentPermission = new ConsentPermission(
-        //         AccessMode.View,
-        //         new AccessPeriod(DateTime.Now, DateTime.Today),
-        //         new DataFrequency(DataFrequencyUnit.Day, 1, 1),
-        //         DateTime.Today);
-        //     var grantedContexts = new List<GrantedContext> {new GrantedContext("12345",
-        //         "batman@tmh")};   
-        //     
-        //     return new ConsentArtefact(consentId,
-        //         DateTime.Now,
-        //         consentPurpose,
-        //         patientReference,
-        //         hipReference,
-        //         hiTypes,
-        //         consentPermission,
-        //         grantedContexts);
         }
     }
 }
