dd12d214e6209712f7955cf3ba0f8809cddb288c
ganesan.arunachalam <ganesan.aruanchalam91@gmail.com>
2020-03-13 14:04:25 +0530

    Copy data from default to Hipservice during runtime

--- src/In.ProjectEKA.DefaultHip/DataFlow/HiTypeDataMap.cs
+++ src/In.ProjectEKA.DefaultHip/DataFlow/HiTypeDataMap.cs
@@ -5,13 +5,13 @@ namespace In.ProjectEKA.DefaultHip.DataFlow
 
     public class HiTypeDataMap
     {
-        private static readonly List<string> obsList = new List<string>() { "observation.json"};
-        private static readonly List<string> diagnosticReportList = new List<string>() { "diagnosticReport.json", "diagnosticReportWithRadiologyImageInline.json"};
+        private static readonly List<string> ObsList = new List<string> { "observation.json"};
+        private static readonly List<string> DiagnosticReportList = new List<string> { "diagnosticReport.json", "diagnosticReportWithRadiologyImageInline.json"};
         private static readonly Dictionary<HiType, List<string>> Map =
             new Dictionary<HiType, List<string>>
             {
-                {HiType.Observation, obsList},
-                {HiType.DiagnosticReport, diagnosticReportList}
+                {HiType.Observation, ObsList},
+                {HiType.DiagnosticReport, DiagnosticReportList}
             };
 
         public Dictionary<HiType, List<string>> GetMap()
