3a3b84d64f650186a06a45ee463ed6cdf1fb00d2
angshuman sarkar <angshuonline@gmail.com>
2020-03-20 11:40:03 +0530

    EKA-150 | setting up sample data for 1 patient (#130)
    * EKA-150 | setting up sample data for 1 patient
    
    * EKA-150 | fixing test

--- src/In.ProjectEKA.DefaultHip/DataFlow/HiTypeDataMap.cs
+++ src/In.ProjectEKA.DefaultHip/DataFlow/HiTypeDataMap.cs
@@ -5,13 +5,29 @@ namespace In.ProjectEKA.DefaultHip.DataFlow
 
     public class HiTypeDataMap
     {
-        private static readonly List<string> ObsList = new List<string> { "observation.json"};
-        private static readonly List<string> DiagnosticReportList = new List<string> { "diagnosticReport.json", "diagnosticReportWithRadiologyImageInline.json"};
+        private static readonly List<string> ObsList 
+            = new List<string> { 
+                "observation.json",
+                "ShriyaTMHFirstVisitConditionAndObs.json",
+                "ShriyaTMHThirdVisitObsWithRef.json"
+            };
+        private static readonly List<string> DiagnosticReportList 
+            = new List<string> { 
+                "diagnosticReport.json", 
+                "diagnosticReportWithRadiologyImageInline.json",
+                "ShriyaTMHFirstVisitDiagnosticReport.json",
+                "ShriyaTMHThirdVisitDiagnosticReports.json"
+            };
+        private static readonly List<string> MedicationRequestList
+            = new List<string> {
+                "ShriyaTMHThirdVisitMedication.json"
+            };
         private static readonly Dictionary<HiType, List<string>> Map =
             new Dictionary<HiType, List<string>>
             {
                 {HiType.Observation, ObsList},
-                {HiType.DiagnosticReport, DiagnosticReportList}
+                {HiType.DiagnosticReport, DiagnosticReportList},
+                {HiType.MedicationRequest, MedicationRequestList}
             };
 
         public Dictionary<HiType, List<string>> GetMap()
