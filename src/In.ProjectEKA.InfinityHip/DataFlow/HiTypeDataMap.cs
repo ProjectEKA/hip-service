e3aebade60c715127c4a8434a45ec98435606e2d
Angshuman Sarkar <angshuonline@gmail.com>
2020-03-20 13:13:23 +0530

    EKA-150 | Adding test data for diagnosticreports

--- src/In.ProjectEKA.InfinityHip/DataFlow/HiTypeDataMap.cs
+++ src/In.ProjectEKA.InfinityHip/DataFlow/HiTypeDataMap.cs
@@ -6,7 +6,12 @@ namespace In.ProjectEKA.DefaultHip.DataFlow
     public class HiTypeDataMap
     {
         private static readonly List<string> ObsList = new List<string> { "observation.json"};
-        private static readonly List<string> DiagnosticReportList = new List<string> { "diagnosticReport.json", "diagnosticReportWithRadiologyImageInline.json"};
+        private static readonly List<string> DiagnosticReportList 
+            = new List<string> { 
+                "diagnosticReport.json", 
+                "diagnosticReportWithRadiologyImageInline.json",
+                "ShriyaInfinitySecondVisitDiagnositicReports.json"
+            };
         private static readonly Dictionary<HiType, List<string>> Map =
             new Dictionary<HiType, List<string>>
             {
