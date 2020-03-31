84ff5ce924f84ccab1bb022c2148c005a8b7e63d
Angshuman Sarkar <angshuonline@gmail.com>
2020-03-31 21:39:14 +0530

    EKA-150 | Fixing data for Patient RVH1003

--- test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
+++ test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
@@ -20,15 +20,15 @@ namespace In.ProjectEKA.DefaultHipTest.DataFlow
         [Fact]
         private async void ReturnEntries()
         {
-            var grantedContexts = new List<GrantedContext>() {new GrantedContext("RVH/NCC-1701", "NCP1006")};
-            var hiDataRange = new HiDataRange("2018-07-01T11:47:00.000+0000", "2020-01-02");
+            var grantedContexts = new List<GrantedContext>() {new GrantedContext("RVH1003", "BI-KTH-12.05.0024"), new GrantedContext("RVH1003", "NCP1008")};
+            var hiDataRange = new HiDataRange("2017-12-01T15:43:00.000+0000", "2020-03-31T15:43:19.279+0000"); 
             var hiTypes = new List<HiType>()
                 {HiType.Condition, HiType.Observation, HiType.DiagnosticReport, HiType.MedicationRequest};
             var dataRequest = new DataRequest(grantedContexts, hiDataRange, "/someUrl", hiTypes, "someTxnId", null);
             var entries = await collect.CollectData(dataRequest);
 
             var bundles = entries.Map(s => s.Bundles);
-            bundles.ValueOrDefault().Count().Should().Be(3);
+            bundles.ValueOrDefault().Count().Should().Be(7);
         }
     }
 }
