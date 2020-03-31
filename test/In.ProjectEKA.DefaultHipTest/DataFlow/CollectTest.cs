7ed536386324c5297b7be8ee83e5fad1277ce1bb
Ganesan Arunachalam <ganesan.arunachalam91@gmail.com>
2020-03-31 17:14:49 +0530

    Fix variable name issue (#156)
    Co-authored-by: ganesan.arunachalam <ganesan.aruanchalam91@gmail.com>

--- test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
+++ test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
@@ -1,38 +1,34 @@
-using System;
 using System.Collections.Generic;
-using In.ProjectEKA.DefaultHipTest.DataFlow.Builder;
 using In.ProjectEKA.HipLibrary.Patient.Model;
-using Xunit.Abstractions;
 
 namespace In.ProjectEKA.DefaultHipTest.DataFlow
 {
     using System.Linq;
     using FluentAssertions;
     using In.ProjectEKA.DefaultHip.DataFlow;
-    using In.ProjectEKA.HipServiceTest.DataFlow.Builder;
     using Optional.Unsafe;
     using Xunit;
 
     [Collection("Collect Tests")]
     public class CollectTest
     {
-        private readonly ITestOutputHelper testOutputHelper;
-        static readonly HiTypeDataMap HiTypeDataMap = new HiTypeDataMap();
+        private static readonly HiTypeDataMap HiTypeDataMap = new HiTypeDataMap();
+
         private readonly Collect collect
             = new Collect(HiTypeDataMap);
-
-        public CollectTest(ITestOutputHelper testOutputHelper)
-        {
-            this.testOutputHelper = testOutputHelper;
-        }
 
         [Fact]
         private async void ReturnEntries()
         {
-            var grantedContexts = new List<GrantedContext>() {new GrantedContext("RVH/NCC-1701", "NCP1006")};
+            var grantedContexts = new List<GrantedContext> {new GrantedContext("RVH/NCC-1701", "NCP1006")};
             var hiDataRange = new HiDataRange("2019-01-01", "2020-01-01");
-            var hiTypes = new List<HiType>()
-                {HiType.Condition, HiType.Observation, HiType.DiagnosticReport, HiType.MedicationRequest};
+            var hiTypes = new List<HiType>
+            {
+                HiType.Condition,
+                HiType.Observation,
+                HiType.DiagnosticReport,
+                HiType.MedicationRequest
+            };
             var dataRequest = new DataRequest(grantedContexts, hiDataRange, "/someUrl", hiTypes, "someTxnId", null);
             var entries = await collect.CollectData(dataRequest);
 
