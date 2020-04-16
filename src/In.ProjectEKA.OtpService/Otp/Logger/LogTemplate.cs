5e53bc470100a0cc29ee8aadf7185e521b598584
Neeraj Kumar Mishra <Nexengineer@users.noreply.github.com>
2020-04-16 11:53:22 +0530

    Removing cached files (#190)

--- src/In.ProjectEKA.OtpService/Otp/Logger/LogTemplate.cs
+++
@@ -1,9 +0,0 @@
-namespace In.ProjectEKA.OtpService.Otp.Logger
-{
-    public static class LogTemplate
-    {
-        public static readonly string ExceptionTemplate = "Exception Occured {Exception}";
-        public static readonly string InformationTemplate = "Success {message}";
-        public static readonly string ErrorTemplate = "Error Occured {Error}";
-    }
-}
