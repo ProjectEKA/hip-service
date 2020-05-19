c7c59ed5a8317a21bb4efc960ed64ea05d13a680
Ganesan Arunachalams <ganesan.arunachalam91@gmail.com>
2020-05-19 19:06:34 +0530

    refactor

--- src/In.ProjectEKA.HipService/Discovery/PatientController.cs
+++ src/In.ProjectEKA.HipService/Discovery/PatientController.cs
@@ -1,68 +1,68 @@
-﻿using System;
-using Hangfire;
-using In.ProjectEKA.HipService.Link;
-
-namespace In.ProjectEKA.HipService.Discovery
+﻿namespace In.ProjectEKA.HipService.Discovery
 {
-    using System.Threading.Tasks;
-    using HipLibrary.Patient.Model;
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
+	using System;
+	using Hangfire;
+	using Link;
+	using System.Threading.Tasks;
+	using HipLibrary.Patient.Model;
+	using Microsoft.AspNetCore.Authorization;
+	using Microsoft.AspNetCore.Mvc;
 
-    [Authorize]
-    [Route("patients/discover/carecontexts")]
-    [ApiController]
-    public class DiscoveryController : Controller
-    {
-        private readonly PatientDiscovery patientDiscovery;
+	[Authorize]
+	[Route("patients/discover/carecontexts")]
+	[ApiController]
+	public class DiscoveryController : Controller
+	{
+		private readonly PatientDiscovery patientDiscovery;
 
-        public DiscoveryController(PatientDiscovery patientDiscovery)
-        {
-            this.patientDiscovery = patientDiscovery;
-        }
+		public DiscoveryController(PatientDiscovery patientDiscovery)
+		{
+			this.patientDiscovery = patientDiscovery;
+		}
 
-        [HttpPost]
-        public async Task<ActionResult> Discover(DiscoveryRequest request)
-        {
-            var (discoveryResponse, error) = await patientDiscovery.PatientFor(request);
+		[HttpPost]
+		public async Task<ActionResult> Discover(DiscoveryRequest request)
+		{
+			var (discoveryResponse, error) = await patientDiscovery.PatientFor(request);
 
-            if (error != null) return NotFound(error);
+			if (error != null) return NotFound(error);
 
-            return Ok(discoveryResponse);
-        }
-    }
+			return Ok(discoveryResponse);
+		}
+	}
 
-    [Route("patients/care-contexts/discover")]
-    [ApiController]
-    public class CareContextDiscoveryController : Controller
-    {
-        private readonly PatientDiscovery patientDiscovery;
+	[Route("patients/care-contexts/discover")]
+	[ApiController]
+	public class CareContextDiscoveryController : Controller
+	{
+		private readonly PatientDiscovery patientDiscovery;
 
-        public CareContextDiscoveryController(PatientDiscovery patientDiscovery)
-        {
-            this.patientDiscovery = patientDiscovery;
-        }
+		public CareContextDiscoveryController(PatientDiscovery patientDiscovery)
+		{
+			this.patientDiscovery = patientDiscovery;
+		}
 
-        [HttpPost]
-        public AcceptedResult DiscoverPatientCareContexts(DiscoveryRequest request)
-        {
-            BackgroundJob.Enqueue(() => GetPatientCareContext(request));
-            return Accepted();
-        }
+		[HttpPost]
+		public AcceptedResult DiscoverPatientCareContexts(DiscoveryRequest request)
+		{
+			// should use cancellation token
+			// https://docs.hangfire.io/en/latest/background-methods/using-cancellation-tokens.html
+			BackgroundJob.Enqueue(() => GetPatientCareContext(request));
+			return Accepted();
+		}
 
-        public Task GetPatientCareContext(DiscoveryRequest request)
-        {
-            var (discoveryResponse, errorRepresentation) =
-                patientDiscovery.PatientFor(request).GetAwaiter().GetResult();
-            var timestamp = DateTime.Now.ToUniversalTime().ToString(Constants.DateTimeFormat);
-            GatewayDiscoveryRepresentation response = new GatewayDiscoveryRepresentation(
-                discoveryResponse.Patient,
-                new Guid(),
-                timestamp,
-                request.TransactionId, //TODO: should be reading transactionId from contract
-                errorRepresentation.Error, new Resp(request.RequestId));
-
-            return Task.CompletedTask;
-        }
-    }
+		public async Task GetPatientCareContext(DiscoveryRequest request)
+		{
+			var (discoveryResponse, errorRepresentation) = await patientDiscovery.PatientFor(request);
+			var timestamp = DateTime.Now.ToUniversalTime().ToString(Constants.DateTimeFormat);
+			GatewayDiscoveryRepresentation response = new GatewayDiscoveryRepresentation(
+				discoveryResponse.Patient,
+				new Guid(),
+				timestamp,
+				request.TransactionId, //TODO: should be reading transactionId from contract
+				errorRepresentation.Error,
+				new Resp(request.RequestId));
+			// TODO: call gateway client
+		}
+	}
 }
