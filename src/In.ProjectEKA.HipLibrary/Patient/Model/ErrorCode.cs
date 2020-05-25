bee6e3cb3ce9979d3f5c780e18bbde5744eb0842
nexengineer <nexengineer@gmail.com>
2020-05-25 17:37:20 +0530

    Fixing the errorcode

--- src/In.ProjectEKA.HipLibrary/Patient/Model/ErrorCode.cs
+++ src/In.ProjectEKA.HipLibrary/Patient/Model/ErrorCode.cs
@@ -3,21 +3,21 @@ namespace In.ProjectEKA.HipLibrary.Patient.Model
     public enum ErrorCode
     {
         NoPatientFound = 1000,
-        MultiplePatientsFound,
-        CareContextNotFound,
-        OtpInValid,
-        OtpExpired,
-        OtpGenerationFailed,
-        NoLinkRequestFound,
-        ServerInternalError,
-        DiscoveryRequestNotFound,
-        ContextArtefactIdNotFound,
-        InvalidToken,
-        HealthInformationNotFound,
-        LinkExpired,
-        ExpiredKeyPair,
-        FailedToGetLinkedCareContexts,
-        DuplicateDiscoveryRequest,
-        DuplicateRequestId
+        MultiplePatientsFound = 1001,
+        CareContextNotFound = 1002,
+        OtpInValid = 1003,
+        OtpExpired = 1004,
+        OtpGenerationFailed = 1005,
+        NoLinkRequestFound = 1006,
+        ServerInternalError = 1007,
+        DiscoveryRequestNotFound = 1008,
+        ContextArtefactIdNotFound = 1009,
+        InvalidToken = 1010,
+        HealthInformationNotFound = 1011,
+        LinkExpired = 1012,
+        ExpiredKeyPair = 1013,
+        FailedToGetLinkedCareContexts = 1014,
+        DuplicateDiscoveryRequest = 1015,
+        DuplicateRequestId = 1016
     }
 }
