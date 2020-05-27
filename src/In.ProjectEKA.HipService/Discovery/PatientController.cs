dbf8ab1589ebb3cdd057a143ccc0e4bae5b5b90e
nexengineer <nexengineer@gmail.com>
2020-05-27 19:03:09 +0530

    Removing end points for old api's

--- src/In.ProjectEKA.HipService/Discovery/PatientController.cs
+++ src/In.ProjectEKA.HipService/Discovery/PatientController.cs
@@ -9,31 +9,7 @@ namespace In.ProjectEKA.HipService.Discovery
     using Hangfire;
     using HipLibrary.Patient.Model;
     using Logger;
-    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-
-    [Authorize]
-    [Route("patients/discover/carecontexts")]
-    [ApiController]
-    public class DiscoveryController : Controller
-    {
-        private readonly PatientDiscovery patientDiscovery;
-
-        public DiscoveryController(PatientDiscovery patientDiscovery)
-        {
-            this.patientDiscovery = patientDiscovery;
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> Discover(DiscoveryRequest request)
-        {
-            var (discoveryResponse, error) = await patientDiscovery.PatientFor(request);
-
-            if (error != null) return NotFound(error);
-
-            return Ok(discoveryResponse);
-        }
-    }
 
     [Route("v1/care-contexts/discover")]
     [ApiController]
