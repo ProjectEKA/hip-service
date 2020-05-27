1c6fe46f59866e40e37b8a750c842174de2e861f
Shweta Dongare <shwetadongare95@gmail.com>
2020-05-27 13:20:51 +0530

    G-HIP-39 | Adding v1/consents/hip/notify API

---
+++ src/In.ProjectEKA.HipService/Consent/Model/Notification.cs
@@ -0,0 +1,23 @@
+namespace In.ProjectEKA.HipService.Consent.Model
+{
+    using Common.Model;
+
+    public class Notification
+    {
+        public ConsentArtefact ConsentDetail { get; }
+
+        public string ConsentId { get; }
+
+        public string Signature { get; }
+
+        public ConsentStatus Status { get; }
+
+        public Notification(ConsentArtefact consentDetail, string consentId, string signature, ConsentStatus status)
+        {
+            ConsentDetail = consentDetail;
+            ConsentId = consentId;
+            Signature = signature;
+            Status = status;
+        }
+    }
+}
