53518fb0e4e89b7597904b60599f23d9c700254f
shwetadongare <62641080+shwetadongare@users.noreply.github.com>
2020-06-05 16:13:18 +0530

    G-HIP-39 | Adding v1/consents/hip/notify API (#248)
    * G-HIP-39 | Adding v1/consents/hip/notify API
    
    * Adding testcases
    
    * Pr Fixes
    
    * Minor Fix
    
    Co-authored-by: Neeraj Kumar Mishra <Nexengineer@users.noreply.github.com>
    Co-authored-by: nexengineer <nexengineer@gmail.com>

---
+++ src/In.ProjectEKA.HipService/Consent/ConsentNotificationController.cs
@@ -0,0 +1,49 @@
+namespace In.ProjectEKA.HipService.Consent
+{
+    using System.Threading.Tasks;
+    using Common.Model;
+    using Hangfire;
+    using Microsoft.AspNetCore.Mvc;
+    using Model;
+
+    [Route("v1/consents/hip/notify")]
+    public class ConsentNotificationController : ControllerBase
+    {
+        private readonly IConsentRepository consentRepository;
+
+        private readonly IBackgroundJobClient backgroundJob;
+
+        public ConsentNotificationController(IConsentRepository consentRepository, IBackgroundJobClient backgroundJob)
+        {
+            this.consentRepository = consentRepository;
+            this.backgroundJob = backgroundJob;
+        }
+
+        [HttpPost]
+        public AcceptedResult ConsentNotification([FromHeader(Name = "X-HIP-ID")] string hipId,
+            [FromBody] ConsentArtefactRepresentation consentArtefact)
+        {
+            backgroundJob.Enqueue(() => StoreConsent(consentArtefact));
+            return Accepted();
+        }
+
+        public async Task StoreConsent(ConsentArtefactRepresentation consentArtefact)
+        {
+            var notification = consentArtefact.Notification;
+
+            if (notification.Status == ConsentStatus.GRANTED)
+            {
+                var consent = new Consent(notification.ConsentDetail.ConsentId,
+                    notification.ConsentDetail,
+                    notification.Signature,
+                    notification.Status,
+                    notification.ConsentId);
+                await consentRepository.AddAsync(consent);
+            }
+            else
+            {
+                await consentRepository.UpdateAsync(notification.ConsentId, notification.Status);
+            }
+        }
+    }
+}
