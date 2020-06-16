80de9fe32e4fc55b997fe0c945fd14b60dea4b68
Neeraj Kumar Mishra <Nexengineer@users.noreply.github.com>
2020-06-16 13:04:42 +0530

    G_62 | Adding api for /v1/health-information/hip/request (#270)

---
+++ test/In.ProjectEKA.HipServiceTest/DataFlow/PatientDataFlowControllerTest.cs
@@ -0,0 +1,56 @@
+namespace In.ProjectEKA.HipServiceTest.DataFlow
+{
+    using System;
+    using Builder;
+    using FluentAssertions;
+    using Hangfire;
+    using Hangfire.Common;
+    using Hangfire.States;
+    using HipLibrary.Patient.Model;
+    using HipService.DataFlow;
+    using Microsoft.AspNetCore.Http;
+    using Moq;
+    using Xunit;
+
+    public class PatientDataFlowControllerTest
+    {
+        private readonly Mock<IBackgroundJobClient> backgroundJobClient = new Mock<IBackgroundJobClient>();
+        private readonly Mock<IDataFlow> dataFlow = new Mock<IDataFlow>();
+        private readonly PatientDataFlowController patientDataFlowController;
+
+        public PatientDataFlowControllerTest()
+        {
+            patientDataFlowController = new PatientDataFlowController(dataFlow.Object, backgroundJobClient.Object);
+        }
+
+        [Fact]
+        private void ShouldEnqueueDataFlowRequestAndReturnAccepted()
+        {
+            var gatewayId = TestBuilder.Faker().Random.String();
+            var transactionId = TestBuilder.Faker().Random.Hash();
+            var requestId = TestBuilder.Faker().Random.Hash();
+
+            var healthInformationRequest = TestBuilder.HealthInformationRequest(transactionId);
+            var hiRequest = new HIRequest(healthInformationRequest.Consent,
+                                          healthInformationRequest.DateRange,
+                                          healthInformationRequest.DataPushUrl,
+                                          healthInformationRequest.KeyMaterial);
+
+            var request = new PatientHealthInformationRequest(transactionId, requestId, It.IsAny<DateTime>(), hiRequest);
+            var expectedResponse = new HealthInformationTransactionResponse(transactionId);
+            dataFlow.Setup(d => d.HealthInformationRequestFor(healthInformationRequest, gatewayId))
+                .ReturnsAsync(
+                    new Tuple<HealthInformationTransactionResponse, ErrorRepresentation>(expectedResponse, null));
+
+            var response = patientDataFlowController.HealthInformationRequestFor(request, gatewayId);
+
+            backgroundJobClient.Verify(client => client.Create(
+                It.Is<Job>(job => job.Method.Name == "HealthInformationOf" && job.Args[0] == request),
+                It.IsAny<EnqueuedState>()
+            ));
+
+            dataFlow.Verify();
+            response.StatusCode.Should().Be(StatusCodes.Status202Accepted);
+        }
+    }
+}
