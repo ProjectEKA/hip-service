cd6dff92974014d0a1afba45c3057af23b1f1b62
morgan amouroux <morgan.amouroux@thoughtworks.com>
2020-07-13 15:50:42 +0700

    HIP13 | Gwan Morgan | Add a test to check the data acess layer is sending the request to the right uri

--- test/In.ProjectEKA.HipServiceTest/OpenMrs/OpenMrsClientTest.cs
+++ test/In.ProjectEKA.HipServiceTest/OpenMrs/OpenMrsClientTest.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
+using In.ProjectEKA.HipService.Common;
+using Moq;
+using Moq.Protected;
 using Xunit;
 
 namespace In.ProjectEKA.HipServiceTest.OpenMrs
@@ -13,24 +18,96 @@ namespace In.ProjectEKA.HipServiceTest.OpenMrs
         [Trait("Category", "Infrastructure")]
         public async Task ShouldGetPatientDataRealCallAsync()
         {
-        //Given
-        // Disable SSL verification in text only
-        var handler = new HttpClientHandler()
-        {
-            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-        };
-        var httpClient = new HttpClient(handler);
-        var openmrsConfiguration = new OpenMrsConfiguration {
-            Url = "https://192.168.33.10/openmrs/",
-            Username = "superman",
-            Password = "Admin123"
+            //Given
+            // Disable SSL verification in text only
+            var handler = new HttpClientHandler()
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
             };
-        var openmrsClient = new OpenMrsClient(httpClient, openmrsConfiguration);
-        //When
-        var response = await openmrsClient.GetAsync("ws/fhir2/Patient");
-        //Then
-        response.Should().NotBeNull();
-        response.StatusCode.Should().Be(200);
+            var httpClient = new HttpClient(handler);
+            var openmrsConfiguration = new OpenMrsConfiguration {
+                Url = "https://192.168.33.10/openmrs/",
+                Username = "superman",
+                Password = "Admin123"
+                };
+            var openmrsClient = new OpenMrsClient(httpClient, openmrsConfiguration);
+            //When
+            var response = await openmrsClient.GetAsync("ws/fhir2/Patient");
+            //Then
+            response.Should().NotBeNull();
+            response.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task ShouldReturnWhenCredentialsFailed()
+        {
+            //Given
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var openmrsConfiguration = new OpenMrsConfiguration
+            {
+                Url = "https://someurl/openmrs/",
+                Username = "someusername",
+                Password = "somepassword"
+            };
+            var openmrsClient = new OpenMrsClient(httpClient, openmrsConfiguration);
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest
+                })
+                .Verifiable();
+
+            //When
+            var response = await openmrsClient.GetAsync("path/to/resource");
+            //Then
+            response.Should().NotBeNull();
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task ShouldCheck()
+        {
+            //Given
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Loose);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var openmrsConfiguration = new OpenMrsConfiguration
+            {
+                Url = "https://someurl/openmrs/",
+                Username = "someusername",
+                Password = "somepassword"
+            };
+            var openmrsClient = new OpenMrsClient(httpClient, openmrsConfiguration);
+
+            var wasCalledWithTheRightUri = false;
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .Callback<HttpRequestMessage, CancellationToken>((response, token) =>
+                {
+                    if (response.RequestUri.AbsoluteUri == "https://someurl/openmrs/path/to/resource")
+                    {
+                        wasCalledWithTheRightUri = true;
+                    }
+                })
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK
+                })
+                .Verifiable();
+
+            //When
+            var response = await openmrsClient.GetAsync("path/to/resource");
+            //Then
+            wasCalledWithTheRightUri.Should().BeTrue();
         }
     }
 }
