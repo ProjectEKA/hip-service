fe279046e5991cb7bfdf4889e9608bcac8b91dd6
morgan amouroux <morgan.amouroux@thoughtworks.com>
2020-07-15 15:52:06 +0700

    HIP13 | Gwan Morgan | Moved constant into static class and clean namespaces in code

---
+++ src/In.ProjectEKA.HipService/OpenMrs/DiscoveryPathConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace In.ProjectEKA.HipServiceTest.OpenMrs
+{
+    public static class DiscoveryPathConstants
+    {
+        public static readonly string OnPatientPath = "ws/fhir2/Patient";
+    }
+}
