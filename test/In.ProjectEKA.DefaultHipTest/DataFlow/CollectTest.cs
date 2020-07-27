2aa0f401a2259164004e8769e7ffab369a065602
Angshuman Sarkar <angshuonline@gmail.com>
2020-07-27 16:19:46 +0530

    EKA-726 | adding data for different care context for Navjot

--- test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
+++ test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
@@ -82,7 +82,7 @@ namespace In.ProjectEKA.DefaultHipTest.DataFlow
                 "sometext");
 
             var entries = await collect.CollectData(dataRequest);
-            entries.ValueOrDefault().CareBundles.Count().Should().Be(11);
+            entries.ValueOrDefault().CareBundles.Count().Should().Be(17);
         }
     }
 }
