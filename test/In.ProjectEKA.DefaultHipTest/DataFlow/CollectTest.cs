e9253b314749546650351072702aebafbe001d39
angshuman sarkar <angshuonline@gmail.com>
2020-07-28 11:37:21 +0530

    EKA-726 | Data Setup (#330)
    * EKA-726 | adding data for different care context for Navjot
    
    * EKA-726 | Adding prescription and diagnosticReport for Hina Patel

--- test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
+++ test/In.ProjectEKA.DefaultHipTest/DataFlow/CollectTest.cs
@@ -47,7 +47,7 @@ namespace In.ProjectEKA.DefaultHipTest.DataFlow
                 "sometext");
 
             var entries = await collect.CollectData(dataRequest);
-            entries.ValueOrDefault().CareBundles.Count().Should().Be(10);
+            entries.ValueOrDefault().CareBundles.Count().Should().Be(16);
         }
 
         [Fact]
@@ -82,7 +82,7 @@ namespace In.ProjectEKA.DefaultHipTest.DataFlow
                 "sometext");
 
             var entries = await collect.CollectData(dataRequest);
-            entries.ValueOrDefault().CareBundles.Count().Should().Be(11);
+            entries.ValueOrDefault().CareBundles.Count().Should().Be(17);
         }
     }
 }
