e807c8683d5128b8d476ce80c2716e5fe38402f9
Apoorva-GA <apoorvaga29@gmail.com>
2020-07-28 13:07:52 +0530

    [G_690] | Meghna/Apoorva | Introducing dummy onAuthInit Api

---
+++ src/In.ProjectEKA.HipService/Link/Model/AuthInit.cs
@@ -0,0 +1,18 @@
+namespace In.ProjectEKA.HipService.Link.Model
+{
+    using System;
+
+    public class AuthInit
+    {
+        public String TransactionId { get; }
+        public AuthType AuthType { get; }
+        public Meta Meta { get; }
+        
+        public AuthInit(string transactionId, AuthType authType, Meta meta)
+        {
+            TransactionId = transactionId;
+            AuthType = authType;
+            Meta = meta;
+        }
+    }
+}
