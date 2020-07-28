d198dbc2944f4df9487c72344c265ff9a99983f6
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-07-28 10:13:41 +0530

    New Hip | Leela Prasanth | Fixes failing unit test

--- test/In.ProjectEKA.DefaultHipTest/Link/PatientRepositoryTest.cs
+++ test/In.ProjectEKA.DefaultHipTest/Link/PatientRepositoryTest.cs
@@ -13,7 +13,7 @@ namespace In.ProjectEKA.DefaultHipTest.Link
         [Fact]
         private void ReturnPatient()
         {
-            const string patientReferenceNumber = "RVH/WBH-1002";
+            const string patientReferenceNumber = "RVH/WBH-10002";
             var testPatient = new Patient
             {
                 PhoneNumber = "+91-7777777777",
@@ -22,7 +22,7 @@ namespace In.ProjectEKA.DefaultHipTest.Link
                 Name = "Navjot Singh",
                 CareContexts = new []
                 {
-                    new CareContextRepresentation("WBCP1007", "National Cancer program"),
+                    new CareContextRepresentation("WBCP1007", "Well Being Cancer program"),
                     new CareContextRepresentation("WB-MHD-01.17.0024", "Dept of Psychiatry - Episode 1"),
                 },
                 YearOfBirth = 2001
