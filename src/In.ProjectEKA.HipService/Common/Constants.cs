e8a785138c207698b6dc84c806d6f2095559c16f
Neeraj Kumar Mishra <Nexengineer@users.noreply.github.com>
2020-07-30 19:13:56 +0530

    G-691 | Adding call back for On-Confirm (#333)

--- src/In.ProjectEKA.HipService/Common/Constants.cs
+++ src/In.ProjectEKA.HipService/Common/Constants.cs
@@ -12,6 +12,8 @@ namespace In.ProjectEKA.HipService.Common
         public const string PATH_LINKS_LINK_CONFIRM = CURRENT_VERSION + "/links/link/confirm";
         public const string PATH_HEALTH_INFORMATION_HIP_REQUEST = CURRENT_VERSION + "/health-information/hip/request";
         public const string PATH_HEART_BEAT = CURRENT_VERSION + "/heartbeat";
+        public const string ON_AUTH_CONFIRM = CURRENT_VERSION + "/users/auth/on-confirm";
+        public const string AUTH_CONFIRM = CURRENT_VERSION + "/users/auth/confirm";
         public const string PATH_ON_AUTH_INIT = "/" + CURRENT_VERSION + "/users/auth/on-init";
 
         public static readonly string PATH_ON_DISCOVER = "/" + CURRENT_VERSION + "/care-contexts/on-discover";
@@ -20,5 +22,6 @@ namespace In.ProjectEKA.HipService.Common
         public static readonly string PATH_CONSENT_ON_NOTIFY = "/" + CURRENT_VERSION + "/consents/hip/on-notify";
         public static readonly string PATH_HEALTH_INFORMATION_ON_REQUEST = "/" + CURRENT_VERSION + "/health-information/hip/on-request";
         public static readonly string PATH_HEALTH_INFORMATION_NOTIFY_GATEWAY = "/" + CURRENT_VERSION + "/health-information/notify";
+        public static readonly string PATH_AUTH_CONFIRM = "/" + CURRENT_VERSION + "/users/auth/confirm";
     }
 }
