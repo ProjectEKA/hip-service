a4ebf08f1d752b211aa9a8b06285efa10450e348
MD Dubey <mddubey409@gmail.com>
2021-03-10 13:31:35 +0530

    Fix errors

--- src/In.ProjectEKA.HipService/Consent/ConsentNotificationController.cs
+++ src/In.ProjectEKA.HipService/Consent/ConsentNotificationController.cs
@@ -53,7 +53,7 @@ namespace In.ProjectEKA.HipService.Consent
                     notification.Status,
                     notification.ConsentId);
                 await consentRepository.AddAsync(consent);
-                var cmSuffix = consent.ConsentArtefact.patientId.Substring(
+                var cmSuffix = consent.ConsentArtefact.patient.id.Substring(
                     patientId.LastIndexOf("@", StringComparison.Ordinal) + 1);                
                 var gatewayResponse = new GatewayConsentRepresentation(
                     Guid.NewGuid(),
@@ -69,7 +69,7 @@ namespace In.ProjectEKA.HipService.Consent
                 if (notification.Status == ConsentStatus.REVOKED)
                 {
                     var consent = await consentRepository.GetFor(notification.ConsentId);
-                    var cmSuffix = consent.ConsentArtefact.patientId.Substring(
+                    var cmSuffix = consent.ConsentArtefact.patient.id.Substring(
                     patientId.LastIndexOf("@", StringComparison.Ordinal) + 1);
                     var gatewayResponse = new GatewayConsentRepresentation(
                         Guid.NewGuid(),
